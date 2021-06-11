c770f83d51c3a01576372f2dd9d039e00035d855
Élise <elise@eiss.fr>
2021-06-12 00:39:37 +0200

    post add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  mode: "jit",
-  purge: {
-    enabled: true,
-    content: [
-      "layouts/**/*.html"
-    ]
-  },
-  darkMode: "media", // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	mode: "jit",
+	purge: {
+		enabled: true,
+		content: [
+			"layouts/**/*.html"
+		]
+	},
+	darkMode: "media", // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/typography'),],
 }
